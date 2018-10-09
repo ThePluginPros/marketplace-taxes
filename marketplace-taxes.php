4e1e9b28a174438f94bb7259c57e10d22998903f
Brett Porcelli <brettporcelli@gmail.com>
2018-10-09 18:51:47 -0400

    chore: add license and copyright information

--- marketplace-taxes.php
+++ marketplace-taxes.php
@@ -14,8 +14,23 @@
  * WC requires at least: 3.0.0
  * WC tested up to:      3.4.5
  *
- * @copyright            Copyright &copy; 2018 The Plugin Pros
+ * @category             Plugin
+ * @copyright            Copyright © 2018 The Plugin Pros, LLC
  * @author               Brett Porcelli
+ * @license              GPL2
+ *
+ * Marketplace Taxes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or any later
+ * version.
+ *
+ * Marketplace Taxes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Marketplace Taxes. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
