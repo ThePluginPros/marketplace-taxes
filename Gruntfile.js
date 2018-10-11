633e38a15c7d70becb9f99dafa34bd1b0be4b4f6
Brett Porcelli <brettporcelli@gmail.com>
2018-10-11 08:41:22 -0400

    build: implement new build and deploy process
    closes #9

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
-module.exports = function( grunt ) {
-    grunt.initConfig( {
+var pkg = require('./package.json');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
         addtextdomain: {
             target: {
                 files: {
@@ -18,7 +20,8 @@ module.exports = function( grunt ) {
                     type: 'wp-plugin',
                     exclude: [
                         'node_modules/.*',
-                        'vendor/.*'
+                        'vendor/.*',
+                        'build/.*'
                     ],
                     potHeaders: {
                         poedit: true,
@@ -30,22 +33,61 @@ module.exports = function( grunt ) {
         },
         uglify: {
             target: {
-                files: [ {
+                files: [{
                     expand: true,
                     cwd: 'assets/js',
-                    src: [ '*.js', '!*.min.js' ],
+                    src: ['*.js', '!*.min.js'],
                     dest: 'assets/js',
-                    rename: function( dst, src ) {
-                        return dst + '/' + src.replace( '.js', '.min.js' )
+                    rename: function (dst, src) {
+                        return dst + '/' + src.replace('.js', '.min.js')
                     }
-                } ]
+                }]
+            }
+        },
+        clean: ['build/'],
+        copy: {
+            target: {
+                expand: true,
+                src: ['assets/**', 'includes/**', 'languages/**', 'vendor/**', 'marketplace-taxes.php', 'readme.txt'],
+                dest: 'build/'
+            }
+        },
+        compress: {
+            target: {
+                options: {
+                    archive: function () {
+                        return 'releases/marketplace-taxes-' + pkg.version + '.zip'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'build/',
+                    src: '**',
+                    dest: 'marketplace-taxes/'
+                }]
+            }
+        },
+        wp_deploy: {
+            deploy: {
+                options: {
+                    plugin_slug: 'marketplace-taxes',
+                    build_dir: 'build',
+                    assets_dir: 'wp-assets'
+                }
             }
         }
-    } );
+    });
 
-    grunt.loadNpmTasks( 'grunt-wp-i18n' );
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks('grunt-wp-i18n');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-wp-deploy');
 
-    grunt.registerTask( 'i18n', [ 'addtextdomain', 'makepot' ] );
-    grunt.registerTask( 'default', [ 'i18n', 'uglify' ] );
+    grunt.registerTask('i18n', ['addtextdomain', 'makepot']);
+    grunt.registerTask('assets', ['uglify']);
+    grunt.registerTask('build', ['i18n', 'assets', 'clean', 'copy', 'compress']);
+    grunt.registerTask('deploy', ['wp_deploy']);
+    grunt.registerTask('default', ['build']);
 };
