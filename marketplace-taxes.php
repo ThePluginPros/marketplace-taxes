d60115c1c5f17b258aed6135c81dc320b396beaf
Brett Porcelli <brettporcelli@gmail.com>
2018-10-23 15:21:44 -0400

    chore: update tested WC version to 3.5.0

--- marketplace-taxes.php
+++ marketplace-taxes.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.4.0
  * Tested up to:         4.9.0
  * WC requires at least: 3.0.0
- * WC tested up to:      3.4.5
+ * WC tested up to:      3.5.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2018 The Plugin Pros, LLC
