c571febb0fe1d39d4747e9cf61e94edbce2236e4
Brett Porcelli <brettporcelli@gmail.com>
2019-11-05 14:13:00 -0500

    fix: bulk edit interface issues
    fixes #32

--- includes/admin/class-mt-admin-bulk-edit.php
+++ includes/admin/class-mt-admin-bulk-edit.php
@@ -17,6 +17,8 @@ class MT_Admin_Bulk_Edit {
     public function __construct() {
         add_action( 'woocommerce_product_bulk_edit_start', array( $this, 'output_fields' ) );
         add_action( 'woocommerce_product_bulk_edit_save', array( $this, 'save_fields' ) );
+        add_action( 'woocommerce_product_bulk_edit_start', array( $this, 'filter_wc_tax_enabled' ), 1000 );
+        add_action( 'woocommerce_product_bulk_edit_end', array( $this, 'remove_wc_tax_enabled_filter' ), 1 );
     }
 
     /**
@@ -39,6 +41,22 @@ class MT_Admin_Bulk_Edit {
         if ( '' !== $category ) {
             update_post_meta( $product->get_id(), 'tax_category', $category );
         }
+    }
+
+    /**
+     * Adds a filter hook to prevent the Tax class and Tax status dropdowns from being displayed in the bulk editor.
+     */
+    public function filter_wc_tax_enabled() {
+        add_filter( 'wc_tax_enabled', '__return_false' );
+    }
+
+    /**
+     * Removes the filter added in `filter_wc_tax_enabled()` just after the bulk editor is rendered.
+     *
+     * @see MT_Admin_Bulk_Edit::filter_wc_tax_enabled()
+     */
+    public function remove_wc_tax_enabled_filter() {
+        remove_filter( 'wc_tax_enabled', '__return_false' );
     }
 
 }
