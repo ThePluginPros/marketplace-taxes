9122e31f6d3986309450a0523e8f748e40f8303f
Brett Porcelli <brettporcelli@gmail.com>
2020-05-23 11:27:28 -0400

    chore: update tested WC version to 4.1

--- marketplace-taxes.php
+++ marketplace-taxes.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.4.0
  * Tested up to:         5.4.0
  * WC requires at least: 3.0.0
- * WC tested up to:      4.0.0
+ * WC tested up to:      4.1.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2020 The Plugin Pros, LLC
